7fe0b770572f3609c486093ddf681894ead27b80
Fred Meyler <fredmeyler@Freds-MacBook-Pro.local>
2020-04-30 16:22:09 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is an attempt to see if one can use machine learning to select assets that performs better than S&P500 during a given period',
+    author='Fred',
+    license='',
+)
